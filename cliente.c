99a5a6d53ee5cf0606639d8ec5ef973d62c2c4fc
Arthur Queiroz <arthuracq@gmail.com>
2016-11-10 13:44:58 -0200

    hello
    Signed-off-by: Arthur Queiroz <arthuracq@gmail.com>

--- cliente.c
+++ cliente.c
@@ -199,8 +199,8 @@ main(int argc, char *argv[])
         fd_set readset;
         FD_ZERO(&readset);
         FD_SET(socketserver, &readset);
-        read_timeout.tv_sec = 1;
-        read_timeout.tv_usec = 1000;
+        read_timeout.tv_sec = 5;
+        read_timeout.tv_usec = 5000;
         int a = 100;
         a = select(socketserver+1,&readset,NULL,NULL,&read_timeout);
         printf("Envio do primeiro token%d\n", a);
